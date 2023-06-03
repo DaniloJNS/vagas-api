fd3559c414b31e26cbeb8dd8d73c6234e563bba0
Danilo Nascimento <dan.silva.13.zx@gmail.com>
2023-06-03 16:01:58 -0300

    feat: organize handlers in separate files

---
+++ handler/index_opening.go
@@ -0,0 +1,13 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IndexOpeningHandler(ctx *gin.Context)  {
+  ctx.JSON(http.StatusOK,  gin.H{
+    "message": "GET Opening",
+  })
+}
